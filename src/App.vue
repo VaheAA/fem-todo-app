15c4fd7652e5a59150e7f03af45c7adbfcdd73ff
VaheAA <vahe.abov@gmail.com>
2022-03-13 19:23:02 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import HeaderBg from './components/HeaderBg.vue';
+</script>
+
+<template>
+  <HeaderBg />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      brightBlue: 'hsl(220, 98%, 61%)',
+      from: 'hsl(192, 100%, 67%)',
+      to: 'hsl(280, 87%, 65%)',
+      veryLightGray: 'hsl(0, 0%, 98%)',
+      veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+      lightGrayishBlue: 'hsl(233, 11%, 84%)',
+      darkGrayishBlue: 'hsl(236, 9%, 61%)',
+      veryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+      veryDarkBlue: 'hsl(235, 21%, 11%)',
+      veryDarkDesBlue: 'hsl(235, 24%, 19%)',
+      lightGrayishBlue: 'hsl(234, 39%, 85%)',
+      lightGrayishHover: 'hsl(236, 33%, 92%)',
+      darkGrayishBlue: 'hsl(234, 11%, 52%)',
+      veryDarkGrayishBlue1: 'hsl(233, 14%, 35%)',
+      veryDarkGrayishBlue2: 'hsl(237, 14%, 26%)'
+    },
+    fontFamily: {
+      sans: ['Josefin Sans', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/images/bg-desktop-dark.jpg')"
+      }
+    }
+  },
+  plugins: []
+};
