700c4cc8dd09ce35fddea3749182e226641f45b3
VaheAA <vahe.abov@gmail.com>
2022-03-14 18:05:13 +0400

    dragndrop functinality

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,37 +1,34 @@
 <template>
   <div
     class="max-w-full bg-veryLightGray dark:bg-veryDarkDesBlue mt-6 rounded-t-md"
-    @dragenter.prevent
-    @dragover.prevent
   >
-    <ul v-if="todos" :todos="todos" @drop="handleDrop">
-      <li v-for="todo in todos" :key="todo.id">
-        <TodoItem
-          :todoText="todo.body"
-          :completed="todo.completed"
-          @delete="deleteTodo(todo.id)"
-          @complete="completeTodo(todo.id)"
-          @dragStart="startDrag($event, todo)"
-        />
-      </li>
-    </ul>
+    <VueDraggableNext
+      v-if="todos"
+      :list="todos"
+      class="dragArea list-group w-full"
+    >
+      <TodoItem
+        class="list-group-item"
+        v-for="todo in todos"
+        :key="todo.id"
+        :todoText="todo.body"
+        :completed="todo.completed"
+        @delete="deleteTodo(todo.id)"
+        @complete="completeTodo(todo.id)"
+      />
+    </VueDraggableNext>
   </div>
 </template>
 
 <script setup>
 import TodoItem from './TodoItem.vue';
+import { VueDraggableNext } from 'vue-draggable-next';
 
 const props = defineProps({
   todos: Array
 });
 
 const emit = defineEmits(['handleDelete', 'handleComplete']);
-
-const startDrag = (event, item) => {
-  event.dataTransfer.dropEffect = 'move';
-  event.dataTransfer.effectAllowed = 'move';
-  event.dataTransfer.setData('id', item.id);
-};
 
 const deleteTodo = (id) => {
   emit('handleDelete', id);
